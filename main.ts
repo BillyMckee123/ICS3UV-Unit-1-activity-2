a9164311b90a6295da20ce2fcff79019dcbca8b3
default <billy.mckee@stu.ocsb.ca>
2025-10-07 13:45:05 +0000

    changed main.ts Hello, World! program

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
+/**
+ * @author Billy Mckee
+ * @version 1.0.0
+ * @date 2025.10.7
+ * @fileoverview This is the traditional "Hello, World!" program.
+ */
+
+// print out the greeting 
 console.log("Hello, World!");
+
+console.log("\nDone.");
